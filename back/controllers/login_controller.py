b5cd470a97d0eeed9e14df0e0e7d2c4a3cef7485
Everton <everton.luan@ufrpe.br>
2026-07-08 09:06:59 -0300

    :sparkles: Feat: Cria controller POST /login integrando schema e service

---
+++ back/controllers/login_controller.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, HTTPException
+from models.login_schema import LoginRequest
+from back.services.login_service import autenticar_usuario
+
+router = APIRouter()
+
+
+@router.post("/login")
+def login(dados: LoginRequest):
+    usuario = autenticar_usuario(dados.login, dados.senha)
+
+    if usuario is None:
+        raise HTTPException(status_code=401, detail="Login ou senha incorretos")
+
+    return usuario
