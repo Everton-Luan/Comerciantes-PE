0110bfc0f96704881d775a071f331a30f39dba08
Everton <everton.luan@ufrpe.br>
2026-07-14 11:36:42 -0300

    :sparkles: Feat: Cria service de listagem e busca de produtos

--- back/teste.py
+++ back/teste.py
@@ -1,4 +1,5 @@
-from services.login_service import autenticar_usuario
+from services.produto_service import listar_produtos, buscar_produto_por_id
 
-print(autenticar_usuario('jonas123', 'errada'))
-print(autenticar_usuario('jonas123', '1234'))
+print(listar_produtos())
+print(buscar_produto_por_id(1))       # troque pelo id de um produto real do seu banco
+print(buscar_produto_por_id(9999))    # id que não existe -> deve vir None
