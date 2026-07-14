d76bd69b6d8f93f2b6d20169957ed8c6a0a18143
Everton <everton.luan@ufrpe.br>
2026-07-14 09:35:02 -0300

    :sparkles: Feat: Cria schemas ProdutoResumo e ProdutoDetalhe

---
+++ back/models/produto_schema.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+class ProdutoResumo(BaseModel):
+    id: int
+    nome: str
+    preco: float
+    foto: str
+
+
+class ProdutoDetalhe(ProdutoResumo):
+    telefone: str
+    local: str
